3889d21ac45f96925dfe5abc8297516eb42745a2
LuckyStar04 <chenxin9204@gmail.com>
2022-11-30 23:33:51 +0800

    change navigationbar structure.

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -24,25 +24,21 @@ watch(() => route.query, () => {
 </script>
 <template>
     <div class="navigations-wrapper">
+        <el-button id="navi-button" type="primary" plain :icon="MoreFilled" style="font-size: 22px;"
+            @click="data.drawer = true" />
         <div class="logo">
             <router-link :to="{ name: 'home' }"><img :src="logopng" /></router-link>
         </div>
-        <div class="blank"></div>
         <div class="right-dock">
             <SearchInput style="width: 160px;margin-right:1rem;"></SearchInput>
-            <el-menu default-active="1" class="el-menu-demo" mode="horizontal" :ellipsis="false" router>
+            <el-menu id="navi-menu" default-active="1" class="el-menu-demo" mode="horizontal" :ellipsis="false" router>
                 <el-menu-item index="0" route="/">首页</el-menu-item>
                 <el-menu-item index="1" route="/articles">文章</el-menu-item>
             </el-menu>
-            <el-switch v-model="isDark" :inline-prompt="true" :active-icon="Sunny" :inactive-icon="Moon"
+            <el-switch id="navi-theme" v-model="isDark" :inline-prompt="true" :active-icon="Sunny" :inactive-icon="Moon"
                 style="margin: 0 1.7rem;" />
             <UserAvatar></UserAvatar>
         </div>
-    </div>
-    <div class="navigations-wrapper-mobile">
-        <el-button type="primary" plain :icon="MoreFilled" style="font-size: 22px;" @click="data.drawer = true" />
-        <router-link :to="{ name: 'home' }"><img :src="logopng" /></router-link>
-        <UserAvatar></UserAvatar>
     </div>
     <el-drawer class="drawer" v-model="data.drawer" title="侧边栏" direction="ltr" :with-header="false" size="70%"
         :close-on-click-modal="true">
@@ -66,15 +62,22 @@ watch(() => route.query, () => {
     margin: 0 auto;
     display: flex;
     justify-content: space-between;
+    align-items: center;
 }
 
-.navigations-wrapper-mobile {
-    z-index: 2010;
-    height: 60px;
+#navi-button {
     display: none;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 10px;
+    width: 45px;
+    height: 40px;
+}
+
+.right-dock>.el-input,
+#navi-menu {
+    display: block;
+}
+
+.right-dock>.el-switch {
+    display: inline-flex;
 }
 
 .right-dock {
@@ -120,20 +123,16 @@ watch(() => route.query, () => {
 
 @media only screen and (max-width: 768px) {
     .navigations-wrapper {
-        display: none;
+        padding: 0 10px;
     }
-
-    .navigations-wrapper-mobile {
+    #navi-button {
         display: flex;
     }
 
-    .navigations-wrapper-mobile img {
-        height: 50px;
-    }
-
-    .navigations-wrapper-mobile .el-button {
-        width: 45px;
-        height: 40px;
+    .right-dock>.el-input,
+    #navi-menu,
+    .right-dock>.el-switch {
+        display: none !important;
     }
 }
 </style>
