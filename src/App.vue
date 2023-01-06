f83621ca56814587d8ab67c5130974c5e643ed6b
LuckyStar04 <chenxin9204@gmail.com>
2023-01-06 14:11:07 +0800

    Add scrollbar to right sidebar

--- src/App.vue
+++ src/App.vue
@@ -86,10 +86,6 @@ const isShowHeader = computed(() => {
     width: 360px;
     flex-shrink: 0;
   }
-
-  .grow-1 :deep(.sidebars) {
-    width: 360px;
-  }
 }
 
 @media only screen and (min-width: 1024px) and (max-width: 1440px) {
@@ -100,10 +96,6 @@ const isShowHeader = computed(() => {
   .grow-1 {
     width: 320px;
     flex-shrink: 0;
-  }
-
-  .grow-1 :deep(.sidebars) {
-    width: 320px;
   }
 }
 
@@ -116,10 +108,6 @@ const isShowHeader = computed(() => {
     width: 280px;
     flex-shrink: 0;
   }
-
-.grow-1 :deep(.sidebars) {
-  width: 280px;
-}
 }
 
 @media only screen and (max-width: 768px) {
--- src/components/SidebarRight.vue
+++ src/components/SidebarRight.vue
@@ -3,8 +3,8 @@ import Categories from './Categories.vue'
 import Tags from './Tags.vue'
 </script>
 <template>
-    <div>
-        <div class="sidebars">
+    <div class="wrapper">
+        <div class="sidebars autohide-scrollbar">
             <Categories></Categories>
             <Tags></Tags>
         </div>
@@ -13,5 +13,26 @@ import Tags from './Tags.vue'
 <style scoped>
 .sidebars {
     position: fixed;
+    height: calc(100vh - 60px);
+    overflow: auto;
+    overflow-y: scroll;
+}
+
+@media only screen and (min-width: 1440px) {
+    .sidebars {
+        width: 360px;
+    }
+}
+
+@media only screen and (min-width: 1024px) and (max-width: 1440px) {
+    .sidebars {
+        width: 320px;
+    }
+}
+
+@media only screen and (min-width: 768px) and (max-width: 1024px) {
+    .sidebars {
+        width: 280px;
+    }
 }
 </style>
