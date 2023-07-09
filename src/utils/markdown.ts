03c5fe69d7a564062a39f851e2c0d4720f482d72
LuckyStar04 <chenxin9204@gmail.com>
2023-07-09 22:34:55 +0800

    Markdown style change.

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -38,7 +38,8 @@ let _base = renderer.code
 
 renderer.code = function (code, infostring, escaped) {
     let base = _base.call(renderer, code, infostring, escaped)
-    return [base.slice(0, 5), `<div class="bsr-head"><div class="bsr-dot"></div><div class="bsr-dot"></div><div class="bsr-dot"></div><div class="bsr-title">${infostring?.toUpperCase()}</div><button class="bsr-copy"><i class="el-icon"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-ea893728=""><path fill="currentColor" d="M768 832a128 128 0 0 1-128 128H192A128 128 0 0 1 64 832V384a128 128 0 0 1 128-128v64a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64h64z"></path><path fill="currentColor" d="M384 128a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64V192a64 64 0 0 0-64-64H384zm0-64h448a128 128 0 0 1 128 128v448a128 128 0 0 1-128 128H384a128 128 0 0 1-128-128V192A128 128 0 0 1 384 64z"></path></svg></i>复制</button></div>`, base.slice(5)].join('')
+    const infostr = infostring || '无'
+    return [base.slice(0, 5), `<div class="bsr-head"><div class="bsr-dot"></div><div class="bsr-dot"></div><div class="bsr-dot"></div><div class="bsr-title">${infostr.toUpperCase()}</div><button class="bsr-copy"><i class="el-icon"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-ea893728=""><path fill="currentColor" d="M768 832a128 128 0 0 1-128 128H192A128 128 0 0 1 64 832V384a128 128 0 0 1 128-128v64a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64h64z"></path><path fill="currentColor" d="M384 128a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64V192a64 64 0 0 0-64-64H384zm0-64h448a128 128 0 0 1 128 128v448a128 128 0 0 1-128 128H384a128 128 0 0 1-128-128V192A128 128 0 0 1 384 64z"></path></svg></i>复制</button></div>`, base.slice(5)].join('')
   }
 
 marked.setOptions({
